402288bde69a66eab413a9e1080d11403806ee72
user-6AnnReGMQR6WwfX1sldH8w <dwiki.nugraha@gdn-commerce.com>
2025-06-12 15:49:11 +0700

    - add loadingfailed event and responserecievedexxtrainfo to receive when response is not finished or failed

--- src/main/java/com/blibli/oss/qa/util/model/RequestResponsePair.java
+++ src/main/java/com/blibli/oss/qa/util/model/RequestResponsePair.java
@@ -2,8 +2,7 @@ package com.blibli.oss.qa.util.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.openqa.selenium.devtools.v137.network.model.Request;
-import org.openqa.selenium.devtools.v137.network.model.Response;
+import org.openqa.selenium.devtools.v137.network.model.*;
 
 
 import java.util.Date;
@@ -11,12 +10,25 @@ import java.util.Date;
 @Getter
 @Setter
 public class RequestResponsePair {
+  private String requestId;
   private Request request;
+  private RequestWillBeSent requestWillBeSent;
+  private RequestWillBeSentExtraInfo requestWillBeSentExtraInfo;
   private Response response;
+  private ResponseReceivedExtraInfo responseReceivedExtraInfo;
+  private LoadingFailed loadingFailed;
+  private LoadingFinished loadingFinished;
   private Date requestOn;
   private String responseBody;
 
   public RequestResponsePair(Request request, Date requestOn, String responseBody) {
+    this.request = request;
+    this.requestOn = requestOn;
+    this.responseBody = responseBody;
+  }
+
+  public RequestResponsePair(String requestId, Request request, Date requestOn, String responseBody) {
+    this.requestId = requestId;
     this.request = request;
     this.requestOn = requestOn;
     this.responseBody = responseBody;
--- src/test/java/com/blibli/oss/qa/util/UnicodeTest.java
+++ src/test/java/com/blibli/oss/qa/util/UnicodeTest.java
@@ -2,7 +2,6 @@ package com.blibli.oss.qa.util;
 
 import com.blibli.oss.qa.util.services.NetworkListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -66,6 +65,34 @@ public class UnicodeTest extends BaseTest {
         String harFile = this.readHarData(HAR_UNICODE_NAME);
         System.out.println(harFile);
         assertTrue(harFile.contains("接口路径不存在 请前往"));
+    }
+
+    @Test
+    public void testUnicodeDemo() throws InterruptedException, IOException {
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        networkListener = new NetworkListener(driver, driver.getDevTools(), HAR_UNICODE_NAME);
+        networkListener.setCharset("UTF-8");
+        networkListener.start();
+        driver.get("https://devtools.glitch.me/network/getstarted.html");
+        Thread.sleep(5000); // make sure the web are loaded
+        driver.quit();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // in the github actions we need add some wait , because chrome exited too slow ,
+        // so when we create new session previous chrome is not closed completly
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        networkListener.createHarFile();
+        String harFile = this.readHarData(HAR_UNICODE_NAME);
+        System.out.println(harFile);
+        assertTrue(!harFile.isEmpty());
     }
 
 
